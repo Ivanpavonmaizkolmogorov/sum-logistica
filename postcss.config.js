cfdf93ed32105bb905181ba622f0771d50be01ae
Ivan Pavon <ivanpavonmaiz@.com>
2025-10-22 23:02:25 +0200

    Initial commit

---
+++ postcss.config.js
@@ -0,0 +1,6 @@
+export default {
+  plugins: {
+    '@tailwindcss/postcss': {}, // ¡Importante que sea este!
+    autoprefixer: {},
+  },
+}
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+ return (
+ <div className="bg-red-500 min-h-screen p-10 flex flex-col items-center justify-center">
+ <h1 className="text-white text-6xl font-extrabold mb-4 animate-pulse">¡FUNCIONA!</h1>
+ <p className="text-yellow-300 text-2xl">Fondo rojo = Tailwind OK</p>
+ </div>
+ );
+}
+export default App;
