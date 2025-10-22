12bd705e45dd5acf4e14d4d8be210af840ee36bf
ivanpavonmaiz <66487540+ivanpavonmaiz@users.noreply.github.com>
2025-10-22 23:35:56 +0200

    Agregada la funcionalidad de recogidas y mejorada la UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}", // ¡Importante esta línea!
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
